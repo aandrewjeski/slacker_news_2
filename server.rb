2c3d8580c9d250a50e4dd841d1bd0ae18ce56275
aandrewjeski <aandrewjeski@gmail.com>
2014-05-29 13:10:32 -0400

    changed gem

--- server.rb
+++ server.rb
@@ -1,11 +1,9 @@
-require 'pg'
 require 'sinatra'
-
 require 'pg'
 
 def db_connection
   begin
-    connection = PG.connect(dbname: 'slacer_news')
+    connection = PG.connect(dbname: 'slacker_news')
 
     yield(connection)
 
@@ -21,7 +19,9 @@ end
 
 def article_exists(params)
   errors = []
-  articles_pg = db_connection("SELECT * FROM articles;")
+  articles_pg = db_connection do |conn|
+  conn.exec("SELECT * FROM articles;")
+  end
   articles = articles_pg.to_a
   articles.each do |article|
     if article['url'] == params
@@ -33,7 +33,9 @@ end
 
 
 get '/' do
-@articles = db_connection("SELECT * FROM articles;")
+@articles = db_connection do |conn|
+  conn.exec("SELECT * FROM articles;")
+end
 @articles = @articles.to_a
 
 erb :home
